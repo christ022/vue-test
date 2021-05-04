e27c3ba18385a5fc1318df854d5db1919f777ea5
christ022 <313680654@qq.com>
2021-05-04 10:28:34 +0800

    注释

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
 module.exports = {
+  // moduleFileExtensions: ["js", "jsx", "json", "vue"],
+  // transform: {
+  //   "^.+\\.vue$": "vue-jest",
+  //   ".+\\.(css|styl|less|sass|scss|svg|png|ttf|woff|woff2)$": "jest-transform-stub",
+  //   "^.+\\.jsx?$": "babel-jest"
+  // },
+  // transformIgnorePatterns: ["/node_modules"],
+  // moduleNameMapper: {
+  //   "^@/(.*)$": "<rootDir>/src/$l"
+  // },
+  // snapshotSerializers: ["jest-serializer-vue"],
+  // testMatch: [
+  //     "**/test/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+  // ],
+  // testURL: "http://localhost/",
   preset: '@vue/cli-plugin-unit-jest'
 }
